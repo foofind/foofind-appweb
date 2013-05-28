586be8d61371db68b0b279932634bea0b570d163
leo <leo@mp2p.net>
2013-05-28 19:13:51 +0200

    Avances en diseño y javascript compatible con IE7.

--- appweb/static/appweb.js
+++ appweb/static/appweb.js
@@ -1,15 +1,50 @@
 document.observe("dom:loaded", function() {
-    $$(".result-action-info > a").each(function (o){
-        $(o).observe("click", function(event) {
-            var element = Event.element(event).up("a");
-            var parent = element.up(".result-item");
-            if (element.hasClassName("button-off")) {
-                element.addClassName("button-on").removeClassName("button-off");
-                parent.addClassName("result-expanded");
+    bubble = $$(".action-report-bubble")[0];
+    bubble.button = false;
+
+    // Mostrar informacion
+    $$(".result-item").each(function (result){
+        $(result).observe("click", function(event) {
+
+            // Clicks fuera de la burbuja
+            if (bubble.button && (!Event.element(event).descendantOf(bubble)))
+            {
+                toggle(bubble.button, bubble, "js-bubble-show");
+                bubble.button = false;
             } else {
-                element.addClassName("button-off").removeClassName("button-on");
-                parent.removeClassName("result-expanded");
+                var stop = true;
+                var result = $(this);
+                var button = Event.element(event);
+                if (!button.match(".button"))
+                    button = button.up(".button");
+                if (result) {
+                    if (!button || button.up(".result-action-info"))
+                        toggle(result.down(".result-action-info .button"), result, "result-expanded");
+                    else if (button.up(".result-action-report")) {
+                        toggle(button, bubble, "js-bubble-show");
+                        var buttonPos = Element.cumulativeOffset(button);
+                        bubble.style.top = buttonPos[1]+"px";
+                        bubble.style.left = buttonPos[0]+"px";
+                        bubble.button = button;
+                    }
+                    else
+                        stop = false;
+                }
             }
+
+            if (stop)
+                Event.stop(event);
         });
     });
 });
+
+
+function toggle(button, parent, className){
+    if (button.hasClassName("button-off")) {
+        button.addClassName("button-on").removeClassName("button-off");
+        parent.addClassName(className);
+    } else {
+        button.addClassName("button-off").removeClassName("button-on");
+        parent.removeClassName(className);
+    }
+}
