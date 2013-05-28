2696c4b08ee9e3b6ec35ad89d152bba92bf405eb
leo <leo@mp2p.net>
2013-05-28 19:13:32 +0200

    Busqueda por tipos funcionando.

--- appweb/blueprints/files.py
+++ appweb/blueprints/files.py
@@ -2,15 +2,13 @@
 """
     Controladores de las páginas de búsqueda y de fichero.
 """
-import urllib, json, unicodedata, random, sys, bson
-from flask import request, render_template, g, current_app, jsonify, flash, redirect, url_for, abort, Markup
-
+import json
+from flask import request, render_template, g, current_app, flash, redirect, url_for
 
 from foofind.blueprints.files import search_files
 from foofind.blueprints.files.helpers import *
 
 from foofind.services import *
-from foofind.utils import url2mid, mid2bin, mid2hex, mid2url, bin2hex, u, canonical_url, logging, is_valid_url_fileid
 from foofind.utils.content_types import *
 from foofind.utils.fooprint import Fooprint
 
@@ -25,31 +23,24 @@ def home():
     return render_template('index.html')
 
 @files.route('/<lang>/<license>/search', methods=["POST"])
-def search(query=None,filters=None):
+def search():
     '''
     Gestiona las URL de busqueda de archivos para la aplicacion.
     '''
+    query=request.form.get("q",None)
+    filetype=request.form.get("t",None)
 
-    url_with_get_params=False
-    if query is None:
-        query=request.form.get("q",None)
-        if not query: #si no se ha buscado nada se manda al inicio
-            flash("write_something")
-            return redirect(url_for("files.home"))
-        else: #sino se reemplazan los espacios que venian antes con un + en el query string y se extraen los filtros
-            query = query.replace("+"," ").replace("/"," ")
-            filters=filters2url(request.form)
-            url_with_get_params=True
+    if not query: #si no se ha buscado nada se manda al inicio
+        flash("write_something")
+        return redirect(url_for(".home"))
 
     query = query.replace("_"," ") if query is not None else None #para que funcionen la busqueda cuando vienen varias palabras
-    dict_filters, has_changed = url2filters(filters) #procesar los parametros
-    dict_filters["src"] = "torrent"
-
-    # obtiene parametros de busqueda de la url
-    if query:
-        args = dict_filters.copy()
-        args["q"] = query
-        g.args=args
+    filters = {"src":"torrent"}
+    if filetype and filetype in CONTENTS_CATEGORY:
+        filters["type"] = [filetype]
+    args = filters.copy()
+    args["q"] = query
+    g.args=args
 
     #sources que se pueden elegir
     fetch_global_data()
@@ -57,7 +48,7 @@ def search(query=None,filters=None):
     sure = False
     total_found=0
 
-    search_results = search_files(query, dict_filters, min_results=request.args.get("min_results",0), last_items=[])
+    search_results = search_files(query, filters, min_results=50, last_items=[], non_group=True)
     files_list = []
     files_info = {}
     for index, afile in enumerate(search_results["files"]):
