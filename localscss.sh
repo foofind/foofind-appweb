7135595b5f23a509f048af91a0d1fc3526d6f35f
leo <leo@mp2p.net>
2013-06-14 14:03:37 +0200

    Generación de scss de localweb simplificada.

--- localscss.sh
+++ localscss.sh
@@ -1,3 +1,3 @@
 . env/bin/activate
-python -mscss < localweb/static/blubster/css/blubster.scss > localweb/static/blubster/css/blubster.css
-python -mscss < localweb/static/foofind/css/foofind.scss > localweb/static/foofind/css/foofind.css
+python -mscss < localweb/static/blubster/css/style.scss > localweb/static/blubster/css/style.css
+python -mscss < localweb/static/foofind/css/style.scss > localweb/static/foofind/css/style.css
